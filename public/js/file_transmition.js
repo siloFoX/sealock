fa0cd13a85484158e75eb932852f6c66184506aa
siloFOX <ladnav0704@gmail.com>
2019-10-17 00:09:43 +0900

    develop : upload mode has a memo

--- public/js/file_transmition.js
+++ public/js/file_transmition.js
@@ -1,17 +1,22 @@
 // todo :
 
 var 
-    test = $$('test')
+    memo = $$('memo')
 
 function modeDropChange() {
     var file_uploader = $$('file-uploader')
 
     if (mode.options[mode.selectedIndex].value == "Upload-mode"){
         file_uploader.className = "hide"
-        test.className = ""
+        memo.className = ""
+
+        alert('Upload mode is activated')
     }
     else{
         file_uploader.className = ""
-        test.className = "hide"
+        memo.className = "hide"
+
+        
+        alert('Update mode is activated')
     }
 }
