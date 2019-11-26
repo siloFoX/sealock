ef768e84c8bae54cd066d3a0eaedf74380586a4f
Raspberry <raspberry6523@gmail.com>
2019-11-26 10:11:24 +0900

    fix : web setting fix

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,6 @@
 
 var express = require('express');
 var router = express.Router();
-
 // GET : root 
 router.get('/', function (req, res) { 
     res.render('index');
--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 const bodyParser = require('body-parser'); // POST : resolution for request body vanishing 
 const express = require('express'); // web framework
 const app = express(); 
+const mongoose = require('mongoose');
 
 //const mongo = require('mongodb') 
 //const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose');
 // CONNECT TO MONGODB SERVER
 mongoose.connect("mongodb://"+"223.194.70.112:27017/SmartProcess", {
    socketTimeoutMS: 0,
@@ -27,15 +27,6 @@ app.use(express.static(__dirname + '/public')); // default directory for both cl
 app.use(bodyParser.urlencoded({extended:true})); // extended url encoding 
 app.use(bodyParser.json()); // default format of body
 
-// Configure router module
-// app.use('/flights', require('./routes/round_index'));
-app.use('/', require('./routes/main'));
-app.use('/table', require('./routes/table'));
-app.use('/process', require('./routes/process'));
-app.use('/file', require('./routes/file'));
-app.use('/memo', require('./routes/memo'));
-app.use('/update', require('./routes/update'));
-
 // Define Model
 // var Flight = require('./models/flight');
 
@@ -45,7 +36,16 @@ app.engine('html', require('ejs').__express);
 // web settings
 app.set('views', __dirname + '/views'); // default directiory for rendering
 app.set('view engine', 'ejs'); // set engine
+app.use('/', require('./routes/main'));
 
-app.listen(process.env.PORT); // port number
+// Configure router module
+// app.use('/flights', require('./routes/round_index'));
+app.use('/table', require('./routes/table'));
+app.use('/process', require('./routes/process'));
+app.use('/file', require('./routes/file'));
+app.use('/memo', require('./routes/memo'));
+app.use('/update', require('./routes/update'));
+
+app.listen(3000); // port number
 
 console.log('Server running on port 3000'); // start sign
