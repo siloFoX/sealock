08ac0f2fc1f9b14beee38bee6b9a5c05b7ac271b
Raspberry <raspberry6523@gmail.com>
2019-11-27 08:07:52 +0900

    feat : add new schema(hardcording....)

--- models/01_Sample_preparation.js
+++ models/01_Sample_preparation.js
@@ -14,4 +14,29 @@ var Sample_preparation = new Schema({
     비고:String
 })
 
+// send one query(insertOne)
+Applying_silver_paste.statics.create = function(payload) {
+    const applying_silver_paste = new this(payload);
+    return applying_silver_paste.save();
+}
+
+//find All
+Applying_silver_paste.statics.findAll = function() {
+    return this.find({});
+}
+
+//return key array
+Applying_silver_paste.method.setIndexes = function() {
+    var json = this.tree;
+    var object = [];
+    for (key in json){
+        object.push(key);
+    }
+    object.pop();
+    object.pop();
+    object.shift();
+    return object;
+}
+
+
 module.exports = mongoose.model('Sample_preparation', Sample_preparation, 'Sample_preparation');
