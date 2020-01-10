9f4146fa007a988b92ef2a5478ba15e9afdfe829
siloFOX <ladnav0704@gmail.com>
2020-01-11 03:43:50 +0900

    hotfix : change shallow to deep copy

--- public/js/table.js
+++ public/js/table.js
@@ -18,116 +18,44 @@ var // DOM controller get by ID
 
 var update_container_changed = false
 
+// why it didn't work??
+
+// function clone(obj) {
+    
+//     if(obj == null || typeof(obj !== 'object'))
+//         return obj;
+
+//     // var copy = obj.constructor()
+//     var copy = {}
+
+//     for(var attr in obj) {
+
+//         if(obj.hasOwnProperty(attr)) {
+
+//             copy[attr] = clone(obj[attr])
+//         }
+//     }
+
+//     return copy
+// }
+
+function clone(obj) {
+
+    return JSON.parse(JSON.stringify(obj))
+}
+
 function render_table(Data = null) {
     var process = dropdown.options[dropdown.selectedIndex].value
     var headers = processess[process]
-    var inner_data = Data
+    var column_size = 10
 
-    if(mode.value == "Update-mode") { // update mode setting
+    if(!Data) {
 
-        if(!inner_data) {
-
-            hot = new Handsontable(container, {
-                data: Handsontable.helper.createSpreadsheetData(1, headers.length),
-                colHeaders: headers,
-                rowHeaders: true,
-                height: 300,
-                width: '100%',
-                minSpareRows: 1,
-                fixedRowsTop : true,
-                manualColumnResize: true,
-                manualRowResize: false,
-                headerTooltips: {
-                    rows: false,
-                    columns: true,
-                    onlyTrimmed: true
-                },
-                licenseKey: "non-commercial-and-evaluation"
-            });
-    
-            hot.clear()
-            return;
-        }
-
-        headers = Object.keys(inner_data[0])
-        var headers_except_id = headers.slice(1)
-
-        var columns = []
-        columns.push({ data : "_id", readOnly : true })
-
-        for (var colHeaderIdx in headers_except_id) {
-            columns.push({ data : headers_except_id[colHeaderIdx] })
-        }
-
-        update_container_changed = false
+        if(mode.value == "Update-mode")
+            column_size = 1
 
         hot = new Handsontable(container, {
-            data: inner_data,
-            colHeaders: headers,
-            columns: columns,
-            rowHeaders: true,
-            height: 520,
-            width: '100%',
-            minSpareRows: 1,
-            manualColumnResize: true,
-            manualRowResize: false,
-            headerTooltips: {
-                rows: false,
-                columns: true,
-                onlyTrimmed: true
-            },
-            licenseKey: "non-commercial-and-evaluation",
-            afterSelection: (row, column, row2, column2, preventScrolling, selectionLayerLevel) => {
-                    select_picture(row, column, row2, column2, headers, hot)
-            }
-        });
-    }
-    else if(inner_data) { // in upload mode 
-        
-        const column_size = 10
-        var last_data = inner_data[inner_data.length - 1]
-
-        var columns = []
-        var row = []
-
-        console.log(last_data)
-
-        for (var i = 0; i < headers.length; i++) {
-        
-            var header = headers[i]
-
-            if(header == "실험날짜" || header == "해당실험기판번호" || header == "실험자명" || header == "사진") 
-                row.push("")
-            else
-                row.push(last_data[header])
-        }
-
-        for (var i = 0; i < column_size; i++) {
-            columns.push(row)
-        }
-
-        hot = new Handsontable(container, {
-            data: columns,
-            colHeaders: headers,
-            rowHeaders: true,
-            height: 300,
-            width: '100%',
-            minSpareRows: 1,
-            fixedRowsTop : true,
-            manualColumnResize: true,
-            manualRowResize: false,
-            headerTooltips: {
-                rows: false,
-                columns: true,
-                onlyTrimmed: true
-            },
-            licenseKey: "non-commercial-and-evaluation"
-        });
-    }
-    else { // upload mode that no data
-
-        hot = new Handsontable(container, {
-            data: Handsontable.helper.createSpreadsheetData(10, headers.length),
+            data: Handsontable.helper.createSpreadsheetData(column_size, headers.length),
             colHeaders: headers,
             rowHeaders: true,
             height: 300,
@@ -145,7 +73,64 @@ function render_table(Data = null) {
         });
 
         hot.clear()
+        return;
     }
+
+    var headers_except_id = Object.keys(Data[0]).slice(1)
+    var table_data = Data
+
+    var columns = []
+    if(mode.value == "Update-mode") {    
+
+        headers = Object.keys(Data[0])
+        columns.push({ data : "_id", readOnly : true })
+        update_container_changed = false
+    }
+    else {
+        var row = {}
+        var rows = []
+
+        for (var idx = 0; idx < headers.length; idx++) {
+
+            var header = headers[idx]
+
+            if(header == "실험날짜" || header == "해당실험기판번호" || header == "실험자명" || header == "사진")
+                row[header] = null
+            else    
+                row[header] = clone(table_data[table_data.length - 1][header])
+        }
+
+        for (var idx = 0; idx < column_size; idx++) {
+            
+            rows.push(clone(row))
+        }
+            
+        table_data = rows
+    }
+
+    for (var colHeaderIdx in headers_except_id) 
+        columns.push({ data : headers_except_id[colHeaderIdx] })
+
+    hot = new Handsontable(container, {
+        data: table_data,
+        colHeaders: headers,
+        columns: columns,
+        rowHeaders: true,
+        height: 520,
+        width: '100%',
+        minSpareRows: 1,
+        manualColumnResize: true,
+        manualRowResize: false,
+        headerTooltips: {
+            rows: false,
+            columns: true,
+            onlyTrimmed: true
+        },
+        licenseKey: "non-commercial-and-evaluation",
+        afterSelection: (row, column, row2, column2, preventScrolling, selectionLayerLevel) => {
+                select_picture(row, column, row2, column2, headers, hot)
+        }
+    });
 }
 
 Handsontable.dom.addEvent(save, 'click', function() {
