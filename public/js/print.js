6673ae8281f30c72a75bdfc2e0632dfd7ec802f2
siloFOX <ladnav0704@gmail.com>
2020-03-03 17:13:27 +0900

    start to make print part

--- public/js/print.js
+++ public/js/print.js
@@ -1,3 +1,30 @@
+var URL = "http://localhost:3000"
 
-console.log("he")
-// window.print()
+var // DOM controller get by ID
+    $$ = function(id) {
+        return document.getElementById(id);
+    },
+
+    container = $$('table'), // table container
+    headers = processess["샘플준비"],
+    column_size = 10
+
+var hot = new Handsontable(container, {
+    data: Handsontable.helper.createSpreadsheetData(column_size, headers.length),
+    colHeaders: headers,
+    rowHeaders: true,
+    height: 300,
+    width: '100%',
+    minSpareRows: 1,
+    fixedRowsTop : true,
+    manualColumnResize: true,
+    manualRowResize: false,
+    headerTooltips: {
+        rows: false,
+        columns: true,
+        onlyTrimmed: true
+    },
+    licenseKey: "non-commercial-and-evaluation"
+});
+
+hot.clear()
