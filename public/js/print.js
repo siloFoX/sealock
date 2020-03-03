289af5767202cb6923ce4fe97a9bf5df2a74d15b
siloFOX <ladnav0704@gmail.com>
2020-03-03 19:45:04 +0900

    Make auto table generator

--- public/js/print.js
+++ public/js/print.js
@@ -5,26 +5,66 @@ var // DOM controller get by ID
         return document.getElementById(id);
     },
 
-    container = $$('table'), // table container
-    headers = processess["샘플준비"],
-    column_size = 10
+    researcher = $$("researcher"),
+    print_info_path = "./json/print.json",
+    record_picture_path = {},
+    record_each_memos = {}
 
-var hot = new Handsontable(container, {
-    data: Handsontable.helper.createSpreadsheetData(column_size, headers.length),
-    colHeaders: headers,
-    rowHeaders: true,
-    height: 300,
-    width: '100%',
-    minSpareRows: 1,
-    fixedRowsTop : true,
-    manualColumnResize: true,
-    manualRowResize: false,
-    headerTooltips: {
-        rows: false,
-        columns: true,
-        onlyTrimmed: true
-    },
-    licenseKey: "non-commercial-and-evaluation"
-});
+$.getJSON(print_info_path, (info) => {
+    
+    researcher.innerHTML = info[0]["실험자명"] + `<small class="text-muted" style = "margin-left : 5px;">연구원</small>`
+    
+    let table_data = []
+    let previous_process = ""
+    let count_num_one_process = 0
 
-hot.clear()
+    for (let idx = 0; idx < info.length; idx++) {
+
+        let each_data = info[idx]
+
+        // 사진이 있으면 사진 가져오기
+        // 사진 없으면 넘어가기 (메모도 마찬가지)
+        
+        if(previous_process && (each_data["공정"] != previous_process || idx == info.length - 1)) {
+
+            if(idx == info.length - 1) {
+
+                table_data.push(each_data)
+                count_num_one_process++
+            }
+
+            let header = processess[reallocater[previous_process]]
+            let table_tag = `<div class = "spreadsheet"><div id = "table` + String(idx) +`" class = "hot"></div></div>`
+
+            header.unshift("공정")
+
+            $("#sheet-controller").append(table_tag)
+
+            let table_location = $$("table" + String(idx))
+            
+            new Handsontable(table_location, {
+                data: table_data,
+                colHeaders: header,
+                rowHeaders: true,
+                width: '100%',
+                minSpareRows: 1,
+                fixedRowsTop : true,
+                manualColumnResize: true,
+                manualRowResize: false,
+                headerTooltips: {
+                    rows: false,
+                    columns: true,
+                    onlyTrimmed: true
+                },
+                licenseKey: "non-commercial-and-evaluation"
+            });
+
+            table_data = []
+            count_num_one_process = 0
+        }
+
+        table_data.push(each_data)
+        previous_process = each_data["공정"]
+        count_num_one_process++
+    }
+})
