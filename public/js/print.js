334ca6ae8d7ed77cf242be67e95e2012095f01f0
siloFOX <ladnav0704@gmail.com>
2020-03-04 08:32:53 +0900

    Fix one data error

--- public/js/print.js
+++ public/js/print.js
@@ -19,13 +19,12 @@ $.getJSON(print_info_path, (info) => {
     let table_num = 0
     let previous_process = ""
     let count_num_one_process = 0
+    let idx = 0
 
-    for (let idx = 0; idx < info.length; idx++) {
+    do {
 
         let each_data = info[idx]
-
-        // 사진이 있으면 사진 가져오기
-        // 사진 없으면 넘어가기 (비고도 마찬가지)
+        
         if(each_data["사진"]) {
             record_picture_path[each_data["공정"] + "_#" + String(count_num_one_process)] = each_data["사진"]
         }
@@ -37,13 +36,16 @@ $.getJSON(print_info_path, (info) => {
         delete each_data["사진"]
         delete each_data["비고"]
         
-        if(previous_process && (each_data["공정"] != previous_process || idx == info.length - 1)) {
+        if((!(info.length - 1) || previous_process) && (each_data["공정"] != previous_process || idx == info.length - 1)) {
 
             if(idx == info.length - 1) {
 
                 table_data.push(each_data)
                 count_num_one_process++
             }
+            
+            if(!(info.length - 1))
+                previous_process = each_data["공정"]
 
             let header = processess[reallocater[previous_process]].slice(0, -2)
             let table_tag = `<div class = "spreadsheet"><div id = "table` + String(table_num) +`" class = "hot"></div></div>`
@@ -78,10 +80,8 @@ $.getJSON(print_info_path, (info) => {
         table_data.push(each_data)
         previous_process = each_data["공정"]
         count_num_one_process++
-    }
 
-    console.log(record_each_memos)
-    console.log(record_picture_path)
+    } while(idx++ < info.length - 1)
 
     renderMemos(record_each_memos)
 })
@@ -94,7 +94,6 @@ function renderMemos(memos) {
         let comment = "<li>" + key + " : " + memos[key] + "</li>"
         all_comment += comment 
     }
-    console.log(all_comment)
     
     memo_controller.innerHTML = all_comment
 }
